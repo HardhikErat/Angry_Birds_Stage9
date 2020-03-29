a7cc22550e002f6457bff5d022410e7cdf5b339a
HardhikErat <58391944+HardhikErat@users.noreply.github.com>
2020-03-29 19:11:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,10 +10,10 @@ var bird, slingshot;
 
 var gameState = "onSling";
 var bg = "sprites/bg1.png";
+var score = 0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
-
+    getBackgroundImg();
 }
 
 function setup(){
@@ -53,6 +53,7 @@ function draw(){
         noStroke();
         textSize(35)
         fill("white")
+        text("Score  " + score, width-300, 50)
     
     Engine.update(engine);
     //strokeWeight(4);
@@ -76,14 +77,13 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();
-    console.log(bird.body.speed);    
+    slingshot.display();    
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+    if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    }
 }
 
 
@@ -93,9 +93,25 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32 && bird.body.speed < 1){
-       bird.trajectory = [];
-       Matter.Body.setPosition(bird.body,{x:200, y:50});
-       slingshot.attach(bird.body);
+    if(keyCode === 32){
+       // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(datetime);
 }
